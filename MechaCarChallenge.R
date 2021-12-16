69698533465d49daf749d67187768fb7530e1c1d
dwright68 <destinwright68@outlook.com>
2021-12-15 19:00:23 -0600

    adding deliverable 2

--- MechaCarChallenge.R
+++ MechaCarChallenge.R
@@ -9,3 +9,13 @@ lm(mpg ~ vehicle_length + vehicle_weight + spoiler_angle + ground_clearance + AW
 
 #use summary to get p value and r squard value
 summary(lm(mpg ~ vehicle_length + vehicle_weight + spoiler_angle + ground_clearance + AWD, data = MechaCar_Mpg))
+
+#PSI Analysis
+
+#import suspension info
+coil_psi <- read.csv(file="Resources/Suspension_Coil.csv",check.names=F,stringsAsFactors = F)
+
+total_summary <- coil_psi %>% summarize(Mean=mean(PSI), Median= median(PSI), Variance= var(PSI), SD= sd(PSI))
+
+# see summarize by lot number 
+lot_summary <- coil_psi %>% group_by(Manufacturing_Lot) %>% summarize(Mean=mean(PSI), Median= median(PSI), Variance= var(PSI), SD= sd(PSI))
